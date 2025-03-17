22fa1dde7e056d7189d993471b2c9e6cc79b4780
Gustavo Galo <gg.gustavo.galo@gmail.com>
2025-03-16 22:15:54 -0300

    refactor: change email service to use SendGrid

--- src/infra/email/email.service.ts
+++ src/infra/email/email.service.ts
@@ -1,25 +1,40 @@
-import { MailerService } from "@nestjs-modules/mailer";
 import { Injectable } from "@nestjs/common";
 import { EmailService as EmailServiceInterface } from "src/domain/integrations/email-service";
+import * as SendGrid from '@sendgrid/mail';
+import { ConfigService } from "@nestjs/config";
+import { renderFile } from "ejs";
+import { join } from "path";
 
 @Injectable()
 export class EmailService implements EmailServiceInterface {
-  constructor(private readonly mailerService: MailerService) {}
+
+  constructor(
+    private readonly config: ConfigService
+  ) {
+    SendGrid.setApiKey(this.config.getOrThrow("SANDGRID_API_KEY"));
+  }
 
   async sendSignUpEmail(data: {
     email: string;
     confirmation_link: string;
   }): Promise<void> {
     const { confirmation_link, email } = data;
+    let html;
 
-    await this.mailerService.sendMail({
+    try {
+      html = await renderFile(join(__dirname, "/templates/sign-up.ejs"), { confirmation_link });
+    } catch (error) {
+      console.error("Error rendering sign-up template: ", error);
+      throw new Error("Error rendering sign-up template");
+    }
+
+    await SendGrid.send({
       to: email,
       subject: "Welcome to our platform!",
-      template: "./sign-up",
-      context: {
-        confirmation_link,
-      },
-    });
+      from: "gg.gustavo.galo@gmail.com",
+      html
+    })
+
   }
 
   async sendForgotPasswordEmail(data: {
@@ -27,14 +42,21 @@ export class EmailService implements EmailServiceInterface {
     reset_link: string;
   }): Promise<void> {
     const { reset_link, email } = data;
+    let html;
 
-    await this.mailerService.sendMail({
+    try {
+      html = await renderFile(join(__dirname, "/templates/forgot-password.ejs"), { reset_link });
+    } catch (error) {
+      console.error("Error rendering sign-up template: ", error);
+      throw new Error("Error rendering sign-up template");
+    }
+
+    await SendGrid.send({
       to: email,
       subject: "Reset Password",
-      template: "./forgot-password",
-      context: {
-        reset_link,
-      },
-    });
+      from: "gg.gustavo.galo@gmail.com",
+      html
+    })
+
   }
 }
